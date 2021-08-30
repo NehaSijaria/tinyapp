9b4dff92dd39934de16aaee62dea9cf1a25ac89a
NehaSijaria <nehasijaria@gmail.com>
2021-08-30 19:43:34 -0400

    url updated

--- express_server.js
+++ express_server.js
@@ -21,6 +21,26 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/hello", (req, res) => {
+  const templateVars = { greeting: 'Hello World!' };
+  res.render("hello_world", templateVars);
+});
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL:"http://www.lighthouselabs.ca/"};
+  res.render("urls_show", templateVars);
 });
